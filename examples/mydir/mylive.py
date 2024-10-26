2e52578e1f5f511140c4e68d601385c339468ab7
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-10-26 10:47:25 -0700

    liveness correct

--- examples/mydir/mylive.py
+++ examples/mydir/mylive.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from collections import namedtuple
+import util
 import cfg
 
 def uses(block):
@@ -8,22 +8,23 @@ def uses(block):
     Compute the set of variables used in `block` before they are assigned to
     """
     gen, defined = set(), set()
-    for instr in cfg.parse_instructions(block):
+    for instr in util.parse_instructions(block):
         for v in instr.uses:
             if not v in defined:
                 gen.add(v)
-        if instr.kill:
-            defined.add(instr.kill)
+        if instr.dest:
+            defined.add(instr.dest)
     return gen
 
 def kill(block):
     """
     Compute set of variables in `block` that are assigned to
     """
-    return set([instr.kill for instr in cfg.parse_instructions(block) if instr.kill])
+    return set([instr.dest for instr in util.parse_instructions(block) if instr.dest])
 
 
-def liveness(block_map, succs, preds, entry):
+def liveness(func_cfg):
+    block_map, succs, preds, entry = func_cfg
     live_in, live_out, uses_map, kill_map = {}, {}, {}, {}
     for name, block in block_map.items():
         uses_map[name] = uses(block)
@@ -33,7 +34,7 @@ def liveness(block_map, succs, preds, entry):
     worklist = set(block_map.keys())
     while worklist:
         name = worklist.pop()
-        out_ = cfg.unionize([live_in[s] for s in succs[name]])
+        out_ = util.unionize([live_in[s] for s in succs[name]])
         in_ = uses_map[name].copy().union(out_ - kill_map[name])
         if in_ != live_in[name] or out_ != live_out[name]:
             live_in[name] = in_
@@ -44,11 +45,11 @@ def liveness(block_map, succs, preds, entry):
 if __name__ == "__main__":
     prog = json.load(sys.stdin)
     for func in prog["functions"]:
-        block_map, succs, preds, entry = cfg.init_cfg(func["instrs"])
-        live_in, live_out = liveness(block_map, succs, preds, entry)
-        for name in block_map:
+        func_cfg = cfg.init_cfg(func["instrs"])
+        live_in, live_out = liveness(func_cfg)
+        for name in func_cfg[0]:
+            in_ = " ".join(sorted([v for v in live_in[name]]))
+            out_ = " ".join(sorted([v for v in live_out[name]]))
             print(f"{name}:")
-            ins = " ".join(live_in[name])
-            outs = " ".join(live_out[name])
-            print(f"  in: {ins}")
-            print(f"  out: {outs}")
+            print(f"  in: {in_}")
+            print(f"  out: {out_}")
